63871614911cdbd382191f83c9df24c5b1034ee8
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-10-18 09:02:02 +0000

    solution serie 1 exercice 1

--- Serie 0/Exercice1.c
+++ Serie 0/Exercice1.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
 typedef struct note{
     float valeur;
     int coefficient;
@@ -8,41 +9,38 @@ typedef struct note{
 typedef struct {
     char nom[20];
     char matricule[20];
-    note notes[3];
+    note *notes;
 }etudiant;
 
-float moyenne(etudiant e){
-    float s1 = 0;
-    float s2 = 0;
+float moyenne(etudiant *e){
+    float s1 = 0, s2 = 0;
     for(int i=0; i<3; i++){
-        s1 += e.notes[i].valeur * e.notes[i].coefficient; 
-        s2 += e.notes[i].coefficient;
+        s1 += e->notes[i].valeur * e->notes[i].coefficient;
+        s2 += e->notes[i].coefficient;
     }
-
     return s1/s2;
 }
 
-int main()
-{
-    etudiant etudiants[3];
+int main() {
+    etudiant *et = new etudiant[3]; 
     for(int i=0; i<3; i++){
-        printf("Donner le nom: ");
-        scanf("%s", &etudiants[i].nom);
-        printf("Donner le matricule: ");
-        scanf("%s", &etudiants[i].matricule);
+        cout<<"Donner le nom: ";
+        cin>>et[i].nom;
+        cout<<"Donner le matricule: ";
+        cin>>et[i].matricule;
+        et[i].notes = new note[3];
         for(int j=0; j<3; j++){
-            printf("Donner la note: ");
-            scanf("%f", &etudiants[i].notes[j].valeur);
-            printf("Donner le cofficient de la note: ");
-            scanf("%d", &etudiants[i].notes[j].coefficient);
-            printf("Donner le nom du module de la note: ");
-            scanf("%s", &etudiants[i].notes[j].module);
+            cout<<"Donner la note: ";
+            cin>>et[i].notes[j].valeur;
+            
         }
+        
+    }
+    for(int i=0; i<3; i++){
+        cout<<"Le nom: "<<et[i].nom<<" Le matricule: "<<et[i].matricule<<" La moyenne est: "<<moyenne(&et[i]);
     }
     
-    for(int i= 0; i<3; i++){
-        printf("Le nom est %s et le matricule %s et sa moyenne est %f\n", 
-        etudiants[i].nom, etudiants[i].matricule, moyenne(etudiants[i]));
-    }
+    delete [] et;
+
     return 0;
 }
